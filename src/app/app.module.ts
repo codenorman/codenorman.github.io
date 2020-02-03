68112ad0e377fa05de906b11df34d89cd736fc2b
Brian Chirgwin <bchirgwin42@gmail.com>
2020-02-02 22:05:19 -0600

    ng update @angular/cdk @angular/material @ngrx/store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
-import {MatButtonModule, MatIconModule, MatMenuModule} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { TopMenuComponent } from './top-menu/top-menu.component';
 import { FooterComponent } from './footer/footer.component';
 
