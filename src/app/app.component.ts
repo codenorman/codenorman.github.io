63a9f089fae3f0185cdba6b4d5c291cf678510c8
Brian Chirgwin <bchirgwin42@gmail.com>
2020-02-18 19:00:41 -0600

    update resources, add filtering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {Meetup} from './meetup/meetup.model';
-import {resources} from '../assets/resources';
+import {resources, tags} from '../assets/resources';
+import {Resource} from './resource/resource.model';
 
 @Component({
   selector: 'app-root',
@@ -9,28 +10,54 @@ import {resources} from '../assets/resources';
 })
 export class AppComponent {
   title = 'Code Norman';
+  meetups: Meetup[];
+  resources: Resource[];
+  filteredResources: Resource[];
+  tags: string[];
+  includeTags: string[];
 
-  meetups: Meetup[] = [
-    {
-      time: 'Tuesday, February 18th 6:30 - ?',
-      location: {
-        name: 'OU Tom Love Innovation Hub',
-        address: 'Three Partners Place, 201 David L Boren Blvd, Norman, OK 73072'
+  constructor() {
+    this.meetups = [
+      {
+        time: 'Tuesday, February 18th 6:30 - ?',
+        location: {
+          name: 'OU Tom Love Innovation Hub',
+          address: 'Three Partners Place, 201 David L Boren Blvd, Norman, OK 73072'
+        },
+        topic: 'JavaScript, TypeScript and Angular'
       },
-      topic: 'JavaScript, TypeScript and Angular'
-    },
 
-    {
-      time: 'Thursday, February 20th 6:00 - ?',
-      location: {
-        name: 'All About Cha',
-        address: '3750 W Main St, Norman, OK 73072'
-      },
-      topic: 'Open Topics'
+      {
+        time: 'Thursday, February 20th 6:00 - ?',
+        location: {
+          name: 'All About Cha',
+          address: '3750 W Main St, Norman, OK 73072'
+        },
+        topic: 'Open Topics'
+      }
+    ];
+    this.resources = resources.sort((a, b) => {
+      return (a.title < b.title) ? -1 : (a.title === b.title) ? 0 : 1;
+    });
+    this.filteredResources = [...this.resources];
+
+    this.tags = tags().sort();
+    this.includeTags = [...this.tags];
+
+  }
+
+  onFilter(e) {
+    const name = e.target.name;
+    const isChecked = e.target.checked;
+    if (isChecked) {
+      this.includeTags = [...this.includeTags, name];
+    } else {
+      this.includeTags = this.includeTags.filter( f => f !== name);
     }
-  ];
-  resources = resources.sort((a, b) => {
-    return (a.title < b.title) ? -1 : (a.title === b.title) ? 0 : 1;
-  });
+    this.filteredResources = [...this.resources.filter(r => {
+      const tags = new Set(r.tags);
+      return this.includeTags.some( tag => tags.has(tag));
+    })]
+  }
 
 }
--- src/assets/resources.ts
+++ src/assets/resources.ts
@@ -31,7 +31,7 @@ export const resources: Resource[] = [
     description: 'Packt Free Learning',
     paid: false,
     upVotes: 0,
-    tags: [],
+    tags: ['book'],
   },
   {
     title: 'JavaScript 30',
@@ -39,7 +39,7 @@ export const resources: Resource[] = [
     description: '30 Day Vanilla JS Coding Challenge',
     paid: false,
     upVotes: 0,
-    tags: [],
+    tags: ['javascript'],
   },
   {
     title: 'JavaScript snippets',
@@ -47,7 +47,7 @@ export const resources: Resource[] = [
     description: 'Curated collection of useful Javascript snippets that you can understand in 30 seconds or less.',
     paid: false,
     upVotes: 0,
-    tags: [],
+    tags: ['javascript'],
   },
   {
     title: 'Code Academy',
@@ -55,7 +55,7 @@ export const resources: Resource[] = [
     description: 'Code Academy',
     paid: false,
     upVotes: 0,
-    tags: [],
+    tags: ['courses'],
   },
   {
     title: 'Code School',
@@ -63,7 +63,7 @@ export const resources: Resource[] = [
     description: 'Code School',
     paid: false,
     upVotes: 0,
-    tags: [],
+    tags: ['courses'],
   },
   {
     title: 'Team Tree House',
@@ -71,7 +71,7 @@ export const resources: Resource[] = [
     description: 'Code School',
     paid: false,
     upVotes: 0,
-    tags: [],
+    tags: ['courses'],
   },
   {
     title: 'ng-conf session videos',
@@ -79,28 +79,110 @@ export const resources: Resource[] = [
     description: 'ng-conf videos of past sessions',
     paid: false,
     upVotes: 0,
-    tags: ['Angular'],
+    tags: ['angular'],
   },
 
+  {
+    title: 'twit.tv',
+    url: 'https://twit.tv/',
+    description: 'podcast network',
+    paid: false,
+    upVotes: 0,
+    tags: ['podcast'],
+  },
+  {
+    title: '.NET Rocks',
+    url: 'https://www.dotnetrocks.com/',
+    description: '.NET Rocks! is a weekly talk show for anyone interested in programming on the Microsoft .NET platform. The shows range from introductory information to hardcore geekiness.',
+    paid: false,
+    upVotes: 0,
+    tags: ['podcast'],
+  },
+  {
+    title: 'Developer On Fire',
+    url: 'https://developeronfire.com/',
+    description: 'podcast: Inspiring software developers tell their stories',
+    paid: false,
+    upVotes: 0,
+    tags: ['podcast'],
+  },
+  {
+    title: 'Devchat.tv',
+    url: 'https://devchat.tv/',
+    description: 'podcast network',
+    paid: false,
+    upVotes: 0,
+    tags: ['podcast'],
+  },
 
+  {
+    title: 'Angular Air',
+    url: 'https://angularair.com/',
+    description: 'A live videocast on YouTube about all things Angular (by Google), a JavaScript platform for building mobile and desktop web applications.',
+    paid: false,
+    upVotes: 0,
+    tags: ['videocast', 'angular'],
+  },
 
+  {
+    title: 'Command_Line Heroes',
+    url: 'https://www.redhat.com/en/command-line-heroes',
+    description: 'An original podcast about the people who transform technology from the command line up. Presented by Red Hat.',
+    paid: false,
+    upVotes: 0,
+    tags: ['podcast'],
+  },
 
-// https://twit.tv/
-// https://www.dotnetrocks.com/
-// https://developeronfire.com/
-// https://devchat.tv/
-// https://angularair.com/
-// https://www.redhat.com/en/command-line-heroes
-// https://risky.biz/
-// https://spec.fm/podcasts/toolsday
-// https://simpleprogrammer.libsyn.com/
-// https://www.packtpub.com/packt/offers/free-learning
-// https://www.manning.com/
-// https://www.humblebundle.com/
-// https://blogs.msdn.microsoft.com/mssmallbiz/2017/07/11/largest-free-microsoft-ebook-giveaway-im-giving-away-millions-of-free-microsoft-ebooks-again-including-windows-10-office-365-office-2016-power-bi-azure-windows-8-1-office-2013-sharepo/
-// https://channel9.msdn.com/
-//
+  {
+    title: 'Risky Business',
+    url: 'https://risky.biz/',
+    description: 'Risky Business was established in February, 2007. Published weekly, the Risky Business podcast features news and in-depth commentary from security industry luminaries. Hosted by award-winning journalist Patrick Gray, Risky Business has become a must-listen digest for information security professionals. We are also known to publish blog posts from time to time.',
+    paid: false,
+    upVotes: 0,
+    tags: ['podcast', 'security'],
+  },
+
+  {
+    title: 'Toolsday',
+    url: 'https://spec.fm/podcasts/toolsday',
+    description: 'A show about the latest in tech tools, tips, and tricks on Tuesdays at 2',
+    paid: false,
+    upVotes: 0,
+    tags: ['podcast'],
+  },
+  {
+    title: 'Manning Publications',
+    url: 'https://www.manning.com/',
+    description: 'ebooks/Books on technology ',
+    paid: false,
+    upVotes: 0,
+    tags: ['book'],
+  },
+  {
+    title: 'Humble Bundle',
+    url: 'https://www.humblebundle.com/',
+    description: 'Bundles of ebooks/software offered for a donation',
+    paid: true,
+    upVotes: 0,
+    tags: ['book', 'software'],
+  },
+
+  {
+    title: 'MS Channel 9',
+    url: 'https://channel9.msdn.com/',
+    description: 'Welcome to Channel 9! We are a humble group of developers and technical enthusiasts within Microsoft who operate this site devoted to including you into the conversations we have.',
+    paid: false,
+    upVotes: 0,
+    tags: ['video'],
+  },
 
 ];
+
+export const tags = () => {
+  const t = resources.reduce((tags, resource) => {
+    return tags.concat(resource.tags);
+  }, []);
+  return Array.from(new Set(t));
+};
 
 
