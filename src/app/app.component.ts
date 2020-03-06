1b0f3d925630a6eb533a46d0ff8ed02a658fdcbf
Brian Chirgwin <bchirgwin42@gmail.com>
2020-03-06 11:29:18 -0600

    update meetups, add future book

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,21 +19,21 @@ export class AppComponent {
   constructor() {
     this.meetups = [
       {
-        time: 'Tuesday, March 3rd, 6:00 - ?',
+        time: 'Tuesday, March 10th, 6:00 - ?',
         location: {
           name: 'Tom Love iHub',
           address: 'Three Partners Place, 201 David L Boren Blvd, Norman, OK 73072'
         },
-        topic: 'Mutable vs Immutable, Haskell, + Open Topics'
+        topic: 'FreeCodeCamp, Answer Questions'
       },
-      // {
-      //       //   time: 'Thursday, February 27th 6:00 - ?',
-      //       //   location: {
-      //       //     name: 'All About Cha',
-      //       //     address: '3750 W Main St, Norman, OK 73072'
-      //       //   },
-      //       //   topic: 'Open Topics'
-      //       // }
+      {
+        time: 'Thursday, March 12th 6:00 - ?',
+        location: {
+          name: 'All About Cha',
+          address: '3750 W Main St, Norman, OK 73072'
+        },
+        topic: 'TBD'
+      }
     ];
     this.resources = resources.sort((a, b) => {
       return (a.title < b.title) ? -1 : (a.title === b.title) ? 0 : 1;
