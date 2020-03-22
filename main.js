fc93847a5314a0776da375dd9eebf16a7d7d9095
Brian Chirgwin <bchirgwin42@gmail.com>
2020-03-22 15:29:31 -0500

    Auto-generated commit

--- main.js
+++ main.js
@@ -848,18 +848,18 @@ var Meetups = [
     {
         time: 'Tuesday, March 24th, 6:00 - ?',
         location: {
-            name: 'Online: https://global.gotomeeting.com/join/823075909',
-            address: 'Get the app now and be ready when your first meeting starts: https://global.gotomeeting.com/install/823075909'
+            name: 'Online: https://global.gotomeeting.com/join/758708085',
+            address: 'Get the app now and be ready when your first meeting starts: https://global.gotomeeting.com/install/758708085'
         },
         topic: 'TBD'
     },
     {
         time: 'Thursday, March 26th 6:00 - ?',
         location: {
-            name: 'Online: https://global.gotomeeting.com/join/316787237',
-            address: 'Get the app now and be ready when your first meeting starts: https://global.gotomeeting.com/install/316787237'
+            name: 'Online: https://global.gotomeeting.com/join/758708085',
+            address: 'Get the app now and be ready when your first meeting starts: https://global.gotomeeting.com/install/758708085'
         },
-        topic: 'TBD'
+        topic: 'OpenCV?, TBD'
     }
 ];
 // {
